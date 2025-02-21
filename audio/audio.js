fa1b1f979e351d9972170c4cfa991ea3cfc9a781
yoonsujinn <yoonsujin2@naver.com>
2025-02-21 22:28:44 +0900

    ewqeq

--- audio/audio.js
+++ audio/audio.js
@@ -1,7 +1,9 @@
 
 document.addEventListener("DOMContentLoaded",function() {
     let bu1 = document.querySelector('.bu_1')
+    let bu2 = document.querySelector('.bu_2')
     const audio_1 = new Audio("./audio_1.mp3");
+    let trackNum = 0; // 현재 재생중인 음원 번호
 
     const audio_arr =  [
         "./audio_1.mp3",
@@ -11,6 +13,21 @@ document.addEventListener("DOMContentLoaded",function() {
 
     bu1.addEventListener('click',function(){
         audio_1.play();
-        console.log('reee')
     })
+
+    bu2.addEventListener('click',function() {
+        playList();
+    })
+
+    function playList() {
+        if(audio_arr.length > trackNum) {
+            let playMusic = new Audio(audio_arr[trackNum])
+            playMusic.play();
+            playMusic.onended = () => {
+                trackNum++;
+                playList()
+            }
+        }
+    }
+    
 })
