118b8277f7ab4775790fb998c7ac2cb2cf6794b4
yoonsujinn <yoonsujin2@naver.com>
2025-03-04 14:18:32 +0900

    ㄷㅈㅂㄷㅂㅂ

--- 한입 크기로 잘라먹는 리액트 강의/section2/chapter12.js
+++ 한입 크기로 잘라먹는 리액트 강의/section2/chapter12.js
@@ -13,39 +13,36 @@
 
 // 음식 주문!!
 function orderfood(callback) {
-    console.log('주문받음')
+    console.log('orderfood 호출됨')
     setTimeout(() => {
-        const food = "떡복이"
-        callback(food);
+        const food = "떡복이"  //orderfood함수에서 food변수를 생성한다.
+        callback(food); //전달받은 콜백함수가 호출되면서 food의 값을 인자로 전달한다.
     }, 3000);
 }
 
 //음식 식히기
-function coldownFoot(food, callback) {
-    console.log(`${food} 음식 식히기 시작!`)
+function coldownFoot(food, callback) { //떡볶이가 전달되었다.
+    console.log(`${food} - 전달받은 음식 식히기 시작!`)
     setTimeout(() => {
         const cooldwnedFood = `식은 ${food}`
         callback(cooldwnedFood)
     }, 2000);
 }
 //음식냉동하기
-function freezeFood (food, callback) {
+function freezeFood (food, callback) { //식은 떡볶이가 전달 되었다.
     setTimeout(()=> {
-        const freezedFood = `냉동된${food}`
+        const freezedFood = `냉동된${food}` //냉동된 식은 떡볶이가 되었다.
         callback(freezedFood)
     },1600)
 }
 
 
-orderfood((food)=> {
-    //음식주문
-    console.log(food)
-        //음식 식힘
-        coldownFoot(food, (cooldwnedFood) => {
-            console.log(cooldwnedFood)
-                //음식냉동
-                freezeFood(cooldwnedFood, (freezedFood) => {
-                    console.log(freezedFood)
+orderfood((food)=> { //음식주문
+    console.log(food) // 떡볶이가 전달되었다.
+        coldownFoot(food, (cooldwnedFood) => { //음식 식힘
+            console.log(cooldwnedFood)  //식은 떡볶이가 전달되었다.
+                freezeFood(cooldwnedFood, (freezedFood) => { //음식 냉동
+                    console.log(freezedFood) //냉동된 식은 떡볶이가 되었다.
                 });
         })
 })
---
+++ 한입 크기로 잘라먹는 리액트 강의/section2/chapter13.js
@@ -0,0 +1,5 @@
+const promise = new Promise (() => {
+    //비동기 작업을 실행하는 함수
+    //executeor라고 부른다.
+
+})
