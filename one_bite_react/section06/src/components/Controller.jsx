638c0d2086a9e4b7daa391ee57df97fe7b824081
yoonsujin <yoonsujin2@naver.com>
2026-02-18 18:45:15 +0900

    section06

---
+++ one_bite_react/section06/src/components/Controller.jsx
@@ -0,0 +1,28 @@
+const Controller = ({ onClickButton }) => {
+
+  console.log(onClickButton)
+  return (
+    <div>
+      <button onClick={() => {
+        onClickButton(-1)
+      }} >-1</button>
+      <button onClick={() => {
+        onClickButton(-10)
+      }} >-10</button>
+      <button onClick={() => {
+        onClickButton(-100)
+      }} >-100</button>
+      <button onClick={() => {
+        onClickButton(+100)
+      }} >+100</button>
+      <button onClick={() => {
+        onClickButton(+10)
+      }} >+10</button>
+      <button onClick={() => {
+        onClickButton(+1)
+      }} >+1</button>
+    </div>
+
+  )
+}
+export default Controller;
